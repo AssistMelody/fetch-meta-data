e308e56ce2ca818496734bdaf320dd49dbec89ff
yuanShen <assistmelody@foxmail.com>
2025-06-11 09:13:58 +0800

    feat：添加爬取

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { resolve, parse } from "node:path";
 import { readdir, writeFile } from "node:fs/promises";
 import { glob } from "glob";
 import { image } from "image-downloader";
+import * as cheerio from "cheerio";
 const sourceFolder = resolve(process.cwd(), "assets");
 
 const saveXML = async (data, path, name) => {
@@ -20,23 +21,38 @@ const saveImage = async (url, path) => {
   });
 };
 
+const fetchPage = async (metaInfo, sn) => {
+  const res = await fetch("https://tw.jav321.com/video/mukc00083").then((res) =>
+    res.text()
+  );
+  const $ = cheerio.load(res);
+  const poster = $(
+    "body > div:nth-child(5) > div.col-md-7.col-md-offset-1.col-xs-12 > div:nth-child(1) > div.panel-body > div:nth-child(1) > div.col-md-3 > img"
+  ).attr("src");
+  const aaa = $("body > div:nth-child(5) > div.col-md-3 > div").map(
+    function () {
+      if (cheerio.load(this)("img")[0]) {
+        return cheerio.load(this)("img")[0].attribs.src;
+      }
+    }
+  );
+  return [poster, ...Array.from(aaa)];
+};
+
 const fetchMetaData = async (folder) => {
-  const metaInfo = {
-    a: {
-      v: {
-        c: "哈哈 a",
-      },
-    },
-  };
+  const metaInfo = {};
 
   const targetFolder = resolve(sourceFolder, folder);
   const medias = await glob(`${targetFolder}/*.{mp4,mkv}`, { nodir: true });
+
   if (medias.length > 0) {
     const { name } = parse(medias[0]);
-    await saveImage(
-      "https://i-blog.csdnimg.cn/blog_migrate/786e3e2caf25c5d1abe665492f8cb69f.png",
-      targetFolder,
-    );
+    const result = await fetchPage(metaInfo, name);
+    for await (const element of result) {
+      saveImage(element, targetFolder);
+    }
+    // https://w.javtxt.top/search?type=id&q=MUKC-083 => https://w.javtxt.top/v/492258
+    //
     await saveXML(metaInfo, targetFolder, name);
   }
 
